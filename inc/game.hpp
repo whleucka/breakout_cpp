52f17e790f66872410bc55cec1cfd43be4eb62f7
William Hleucka <william.hleucka@gmail.com>
2024-06-27 07:20:43 -0600

    fix: remove unused method

--- inc/game.hpp
+++ inc/game.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
 #include "ball.hpp"
-#include "brick.hpp"
 #include "player.hpp"
 #include "bricks.hpp"
 #include "pulse/engine.hpp"
 
 #include <string>
-#include <vector>
 
 class Game : public Pulse::Engine {
 public:
@@ -23,7 +21,6 @@ private:
 
   std::string formatScore() const;
   void nextLevel();
-  bool checkEndLevel(const std::vector<Brick *> &bricks);
   void displayScore() const;
 
   void setupGame() override;
